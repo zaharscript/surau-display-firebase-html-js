a96ce3d751d95a0f10bbb37b12a508430911d4cd
unknown <zahratulnazirah@gmail.com>
2026-03-15 13:02:55 +0800

    rename infaq_3.jpeg to infaq_2.jpeg

--- js/script.js
+++ js/script.js
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
       "img/surau_poster/surau_qr.jpeg",
       "img/surau_poster/infaq_1.jpeg",
       "img/surau_poster/malam_penghargaan.jpeg",
-      "img/surau_poster/infaq_3.jpeg"
+      "img/surau_poster/infaq_2.jpeg"
     ];
 
     if (!sliderWrapper) return;
